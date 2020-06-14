d65597fefa773e3348be73a14b6064d256be34bc
Carlos Magalhaes <magalhaescarlosedu@gmail.com>
2020-06-14 03:05:57 -0300

    Revisão da home

--- caminhoes_app/lib/screens/home_screen.dart
+++ caminhoes_app/lib/screens/home_screen.dart
@@ -1,9 +1,26 @@
+import 'package:caminhoes_app/models/user_model.dart';
+import 'package:caminhoes_app/screens/login_screen.dart';
 import 'package:caminhoes_app/tabs/home_tab.dart';
+import 'package:caminhoes_app/tabs/places_tab.dart';
 import 'package:caminhoes_app/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return !UserModel.of(context).isLoggedIn()?HomeLogada(pageController: _pageController,):LoginScreen();
+  }
+}
+
+class HomeLogada extends StatelessWidget {
+  const HomeLogada({
+    Key key,
+    @required PageController pageController,
+  }) : _pageController = pageController, super(key: key);
+
+  final PageController _pageController;
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +42,7 @@ class HomeScreen extends StatelessWidget {
             centerTitle: true,
           ),
           drawer: CustomDrawer(_pageController),
-          body: Container(color: Colors.pink,),
+          body: PlacesTab(),
         ),
         Scaffold(
           appBar: AppBar(
